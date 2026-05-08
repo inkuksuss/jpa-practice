f6a3bcf73ed58c5ee479ff3f0d63aadb0f995c42
inkuksuss <inkuksuss@naver.com>
2026-05-08 18:18:06 +0900

    init

---
+++ src/main/java/com/example/jpa_pratice/JpaPraticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.jpa_pratice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaPraticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaPraticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jpa_pratice/JpaPraticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jpa_pratice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaPraticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
